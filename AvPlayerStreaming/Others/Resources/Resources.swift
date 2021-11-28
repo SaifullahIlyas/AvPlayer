70ca7d866a28614136dfa33a9da91f5135a458da
Saifullah ilyas <saifullahilyas907@gmail.com>
2021-11-28 10:41:23 +0500

    SetUp Project Architecture

---
+++ AvPlayerStreaming/Others/Resources/Resources.swift
@@ -0,0 +1,15 @@
+//
+// Resources.swift
+//  AvPlayerStreaming
+//
+//  Created by SAIF ULLAH on 28/11/2021.
+//
+
+import Foundation
+
+class Resources  {
+    static let strings  = StringResources()
+}
+class StringResources {
+ 
+}
